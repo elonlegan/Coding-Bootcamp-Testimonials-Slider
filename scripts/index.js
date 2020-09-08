f814a031913422c347b009d0e0a02bb83f7086cf
Angel Leon <elonlegan@gmail.com>
2020-09-08 15:10:22 -0500

    basic structure and funcionality done

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const textContainer = document.getElementsByClassName("text-container");
+const image = document.getElementsByClassName("image");
+
+function changeSlide() {
+  console.log("nsnakjfnkanf");
+  textContainer[0].classList.toggle("none");
+  textContainer[1].classList.toggle("none");
+  image[0].classList.toggle("none");
+  image[1].classList.toggle("none");
+}
