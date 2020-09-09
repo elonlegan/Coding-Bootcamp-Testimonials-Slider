c930573b910af5e85bf6ff2266822b24341160ae
Angel Leon <elonlegan@gmail.com>
2020-09-08 22:05:20 -0500

    done

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ const textContainer = document.getElementsByClassName("text-container");
 const image = document.getElementsByClassName("image");
 
 function changeSlide() {
-  console.log("nsnakjfnkanf");
   textContainer[0].classList.toggle("none");
   textContainer[1].classList.toggle("none");
   image[0].classList.toggle("none");
